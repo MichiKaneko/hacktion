2a7e4d40fb75954b4b1fe8455d8ec1ca9ccd1710
MichiKaneko <michihito1005@gmail.com>
2023-01-08 14:58:17 +0900

    loginコマンドを追加

--- cmd/login.go
+++ cmd/login.go
@@ -4,10 +4,19 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
+
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
@@ -20,20 +29,55 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("login called with args: ", args)
+		email, err := cmd.Flags().GetString("email")
+		if err != nil {
+			fmt.Println(err)
+		}
+		password, err := cmd.Flags().GetString("password")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		endpoint := "https://hacktion.app.mode-co.jp/api/v1/login"
+
+		login := Login{
+			Email:    email,
+			Password: password,
+		}
+
+		jsonBytes, err := json.Marshal(login)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonBytes))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+
+		body, err := io.ReadAll(resp.Body)
+
+		fmt.Println("response Body:", string(body))
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(loginCmd)
+	loginCmd.Flags().StringP("email", "e", "", "email")
+	loginCmd.Flags().StringP("password", "p", "", "password")
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
